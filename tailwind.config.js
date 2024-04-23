d9bfcb27f930121b4d810dcca1ef0114f487640e
iamgx <imaxing@126.com>
2024-04-23 09:56:48 +0800

    fix(layout): layout css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,14 @@ module.exports = {
   mode: 'jit',
   darkMode: 'media',
   purge: {
-    content: ['src/**/*.{vue,js,ts,jsx,tsx}']
+    content: ['src/**/*.{vue,js,ts,jsx,tsx}', 'node_modules/@iamgx/el-layout/**/*.{vue,js,ts,jsx,tsx}']
   },
   theme: {
     color: { primary: '#3B82F6' },
     extend: {
+      width: { aside: '63px' },
+      height: { aside: '63px' },
+      colors: { primary: '#1F2124' },
       boxShadow: { debug: '0 0 0 1px red' }
     }
   },
